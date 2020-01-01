e2c830c5132fc89bede64ad65beb1b3541aea5c3
christian-brindley <christian.brindley@gmail.com>
2020-01-01 09:16:35 +0000

    Create popRegister.groovy

---
+++ nodes/popRegister.groovy
@@ -0,0 +1,87 @@
+import org.forgerock.http.protocol.*
+import org.forgerock.json.jose.utils.Utils
+import groovy.json.JsonOutput
+import java.util.Date;
+  
+def userName = sharedState.get("username");
+  
+def idmUser = "openidm-admin";
+def idmPass = "openidm-admin";
+
+
+def error = false;
+
+  
+Request request = new Request()
+request.setMethod('GET');
+request.setUri("https://idm.authdemo.org/openidm/managed/user?_queryFilter=userName+eq+'" + userName + "'")
+request.getHeaders().add("X-OpenIDM-Username",idmUser);
+request.getHeaders().add("X-OpenIDM-Password",idmPass);
+
+def response = httpClient.send(request).get();
+def String responseContent = response.getEntity();
+def responseStatus = response.getStatus();
+
+logger.error("status " + responseStatus);
+logger.error("entity " + responseContent);
+
+if (!responseStatus.toString().contains("200")) {
+  logger.error("Failed to get user id from IDM");
+  error = true;
+}
+
+if (!error) {
+
+   def responseContentBin = Utils.parseJson(responseContent);
+   def userEntry = responseContentBin.result;
+   def id = userEntry._id[0];
+
+   logger.error("id {}",id);
+
+   def requestContent = [
+  
+   ];
+
+   def device = sharedState.get("popDevice");
+   def friendlyName = sharedState.get("popFriendlyName");
+   def Date now = new Date();      
+   def nowEpoch = now;
+  
+   def postBodyBin = [
+     "deviceId": device.deviceId,
+     "friendlyName": friendlyName,
+     "lastUsed": nowEpoch,
+     "registered": nowEpoch,
+     "status": "active",
+     "jwk": device.jwk,
+     "owner": [
+       "_ref": "managed/user/" + id,
+     ]
+   ];
+
+   def postBody = JsonOutput.toJson(postBodyBin);
+
+   logger.error("Sending content {}",postBody);
+   request = new Request();
+   request.setMethod('POST');
+   request.setUri("https://idm.authdemo.org/openidm/managed/device?_action=create");
+   request.getHeaders().add("X-OpenIDM-Username",idmUser);
+   request.getHeaders().add("X-OpenIDM-Password",idmPass);
+   request.getHeaders().add("Content-Type","application/json");
+   request.setEntity(postBody);
+                  
+   response = httpClient.send(request).get();
+   responseContent = response.getEntity();
+   responseStatus = response.getStatus();
+
+   logger.error("status " + responseStatus);
+   logger.error("entity " + responseContent);
+
+   if (!responseStatus.toString().contains("201")) {
+      logger.error("Failed to create device");
+      error = true;
+   }  
+}
+
+
+outcome = error ? "false" : "true";
