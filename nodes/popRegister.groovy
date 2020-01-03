49bc794ee63edd1b7483e51b38eb001123fbfb00
christian-brindley <christian.brindley@gmail.com>
2020-01-03 10:35:56 +0000

    Update popRegister.groovy

--- nodes/popRegister.groovy
+++ nodes/popRegister.groovy
@@ -2,15 +2,32 @@ import org.forgerock.http.protocol.*
 import org.forgerock.json.jose.utils.Utils
 import groovy.json.JsonOutput
 import java.util.Date;
+import org.forgerock.guice.core.InjectorHolder;
+import org.forgerock.openam.secrets.Secrets;
+import org.forgerock.openam.secrets.SecretsProviderFacade;
+import org.forgerock.secrets.Purpose;
   
 def userName = sharedState.get("username");
-  
+ 
+
+def buildString(char[] input) {
+  return String.valueOf(input);
+}
+
+// Get secret for IDM password
+secrets = InjectorHolder.getInstance(Secrets.class);
+
+def idmPass = secrets.getGlobalSecrets()
+   .getNamedSecret(Purpose.PASSWORD,"IDMPassword")
+   .getOrThrowUninterruptibly().revealAsUtf8(this.&buildString); 
+
 def idmUser = "openidm-admin";
-def idmPass = "openidm-admin";
 
 
 def error = false;
 
+
+// Get the uuid for given username
   
 Request request = new Request()
 request.setMethod('GET');
@@ -18,12 +35,13 @@ request.setUri("https://idm.authdemo.org/openidm/managed/user?_queryFilter=userN
 request.getHeaders().add("X-OpenIDM-Username",idmUser);
 request.getHeaders().add("X-OpenIDM-Password",idmPass);
 
+
 def response = httpClient.send(request).get();
 def String responseContent = response.getEntity();
 def responseStatus = response.getStatus();
 
-logger.error("status " + responseStatus);
-logger.error("entity " + responseContent);
+logger.message("status " + responseStatus);
+logger.message("entity " + responseContent);
 
 if (!responseStatus.toString().contains("200")) {
   logger.error("Failed to get user id from IDM");
@@ -36,12 +54,11 @@ if (!error) {
    def userEntry = responseContentBin.result;
    def id = userEntry._id[0];
 
-   logger.error("id {}",id);
+   logger.message("id {}",id);
 
-   def requestContent = [
   
-   ];
-
+   // Add the device with link to user
+  
    def device = sharedState.get("popDevice");
    def friendlyName = sharedState.get("popFriendlyName");
    def Date now = new Date();      
